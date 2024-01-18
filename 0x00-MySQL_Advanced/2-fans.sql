36e05d3535afae17cbc9df08290f5f8aad917c4d
Bante <bantamlak21@gmail.com>
2024-01-18 10:03:39 +0000

    sum instead of count

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Ranks country origin of bands ordered by the number of fans
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
