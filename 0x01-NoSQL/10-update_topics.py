46aaa36041fe97c2e6f6b5723e7ed76e5654592a
Bante <bantamlak21@gmail.com>
2024-01-22 23:52:26 -0800

    task 10 first commit

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+File: 10-update_topics.py
+
+Function:
+---------
+A function that changes all topics of a school document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    parameters:
+    - mongo_collection: pymongo colection object
+    - name (string): The school name to update
+    - topicd (list of strings): List of topics approached in the school
+
+    Returns:
+    - Updated document
+    """
+    return mongo_collection.updateMany(
+            { name: name },
+            { $set: { topics: topics } }
+        )
