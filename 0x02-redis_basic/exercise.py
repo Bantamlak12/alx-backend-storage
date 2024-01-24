f044d6608d91e76c295b0b9f3511cf197fc4d845
Bantamlak <bantamlak29@gmail.com>
2024-01-24 18:45:57 +0300

    storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -27,6 +27,32 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    parameters:
+    - method (Callable): The method to be decorated
+
+    Returns:
+    - Callable: The decorated method
+    """
+    @wraps(method)
+    def wrapper(self, *args):
+        key = method.__qualname__
+        inputs_key = key + ":inputs"
+        outputs_key = key + ":outputs"
+
+        # Log inputs
+        self._redis.rpush(inputs_key, str(args))
+
+        res = method(self, *args)
+
+        # Log outputs
+        self._redis.rpush(outputs_key, str(res))
+
+        return res
+    return wrapper
+
+
 class Cache:
     """ Writing strings to Redis """
     def __init__(self):
@@ -38,6 +64,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         parameters:
